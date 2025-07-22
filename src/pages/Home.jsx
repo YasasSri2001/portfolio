ac83a17ea5ccb7e1017c0cb9c3778d228964a94f
UdulaIndumina <116205513+UdulaIndumina@users.noreply.github.com>
2025-07-23 01:34:14 +0530

    Update Home.jsx and fix profile image path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -157,7 +157,8 @@ const Home = () => {
                 whileHover={{ scale: 1.03 }}
                 className="relative bg-white rounded-full p-2 shadow-xl"
               >
-              <img src="/profile.jpg" alt="Profile" className="rounded-full w-64 h-64 object-cover border-4 border-white" />
+              <img src={`${process.env.PUBLIC_URL}/profile.jpg`} alt="Profile" className="rounded-full w-64 h-64 object-cover border-4 border-white" />
+
 
 
               </motion.div>
